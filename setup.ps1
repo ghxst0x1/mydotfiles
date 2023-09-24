657e195c824f370297f25ffacada9fe30594d9d6
HoLyGhxsT <karthikrofficial7@gmail.com>
2023-09-24 15:00:24 +0530

    Revamped installation script(not tested) Updated PowerShell profile

--- setup.ps1
+++ setup.ps1
@@ -1,21 +1,96 @@
-Write-Host Start -ForegroundColor Cyan
+# Check if running as administrator
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Please run this script as an administrator."
+    Exit
+}
 
-Get-ExecutionPolicy 
+# Enable PowerShell script execution
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Install Windows Package Manager (winget)
+$wingetInstalled = Get-Command winget -ErrorAction SilentlyContinue
+if (-not $wingetInstalled) {
+    Write-Host "Installing Windows Package Manager (winget)..."
+    Invoke-Expression "iex ((New-Object System.Net.WebClient).DownloadString('https://aka.ms/install-powershell.ps1'))"
+} else {
+    Write-Host "Winget Already Installed"
+}
+
+# Install Scoop
+$scoopInstalled = Test-Path $env:USERPROFILE\scoop
+if (-not $scoopInstalled) {
+    Write-Host "Installing Scoop..."
+    Invoke-Expression "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')"
+} else {
+    Write-Host "Scoop Already Installed"
+}
+
+# Install Oh-my-posh
+$wingetInstalled = Get-Command oh-my-posh -ErrorAction SilentlyContinue
+if (-not $wingetInstalled) {
+    Write-Host "Installing Oh-My-Posh..."
+    Invoke-Expression "iex((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))"
+} else {
+    Write-Host "Oh-My-Posh Already Installed"
+}
+
+# Add Scoop buckets
+scoop bucket add extras
+scoop bucket add nerd-fonts
+scoop bucket add versions
+
+# Install packages using Scoop
+$packages = @("7zip","curl", "aria2c", "lsd")
+foreach ($package in $packages) {
+    Write-Host "Installing $package..."
+    scoop install $package
+}
+
+# Install CaskaydiaCove Nerd Font using Scoop
+$nerdFontInstalled = Test-Path "$env:USERPROFILE\scoop\apps\nerd-fonts\current"
+if (-not $nerdFontInstalled) {
+    Write-Host "Installing CaskaydiaCove Nerd Font..."
+    scoop install nerd-fonts/CaskaydiaCove-Nerd-Font
+}
+
+# Install packages using Winget
+$wingetPackages = @(
+    "Git.Git",
+    "Discord.Discord.PTB",
+    "Valve.Steam",
+    "Brave.Brave.Beta",
+    "Microsoft.VisualStudioCode.Insiders",
+    "VideoLAN.VLC",
+    "Logitech.GHUB",
+    "qBittorrent.qBittorrent",
+    "WinSCP.WinSCP",
+    "DuongDieuPhap.ImageGlass")
+foreach ($package in $wingetPackages){
+    Write-Host "Installing $package using Winget..."
+    winget install --id $package -e -q
+}
+
+# Install PowerShell modules
+$modules = @("terminal-icons", "psReadline", "psfzf", "z")
+foreach ($module in $modules) {
+    Write-Host "Installing PowerShell module $module..."
+    Install-Module -Name $module -Force -Scope CurrentUser
+}
+
+#copying files
+Write-Host "Copying config files..."
+Write-Host "Warning!!! This will overwrite current profile configuration" -ForegroundColor Red
+$currentdir = Get-Location
+$destdir = "C:\Users\$env:USERNAME\documents"
+
+Copy-Item -Path $currentdir\PowerShell -Destination $destdir -Recurse -Force
+
+# Check if the copy was successful
+if (Test-Path -Path $destdir\PowerShell) {
+    Write-Host "Folder copied successfully to $destinationFolder"
+} else {
+    Write-Host "Failed to copy folder to $destinationFolder"
+}
 
 
-Write-Host Installing PSFzf -ForegroundColor Yellow
-powershell Install-Module -Name PSFzf
-
-Write-Host Installing PSReadLine -ForegroundColor Yellow
-Install-Module -Name PSReadLine
-
-Write-Host Installing Terminal-Icons -ForegroundColor Yellow
-Install-Module -Name Terminal-Icons
-
-Write-Host Installing z -ForegroundColor Yellow
-Install-Module -Name z
-
-Write-Host Installing Scoop -ForegroundColor Yellow
-irm get.scoop.sh | iex
-
-Write-Host Done -ForegroundColor Green
+Write-Host "Installation complete."
